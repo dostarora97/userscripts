29a974e3edce2e0a6c0fb33f74bc024a40b3cb89
dost.arora97 <dost.arora97@gmail.com>
2021-06-26 07:20:58 +0530

    Remove Bootstarp. App custom logic

--- src/UI.ts
+++ src/UI.ts
@@ -1,63 +1,78 @@
 import {Video} from "./Video";
 
 export class UI {
-    protected dropdownEle: Element;
+    protected dropdownEle: HTMLElement;
 
     constructor(videos: Video[]) {
         this.dropdownEle = this.createdDropdownEle();
         for (let video of videos) {
-            let downloadEle: Element = this.createDownloadEle(video);
-            this.appendDownloadToDropdown(this.dropdownEle, downloadEle);
+            let downloadEle: HTMLElement = this.createDropdownItem(video);
+            this.appendItemToDropdown(this.dropdownEle, downloadEle);
         }
     }
 
-    protected createdDropdownEle(): Element {
-        let dropdownWrapper: Element = document.createElement("div");
-        dropdownWrapper.classList.add("dropdown");
+    protected createdDropdownEle(): HTMLElement {
+        let dropdownWrapper: HTMLElement = document.createElement("div");
+        dropdownWrapper.classList.add("dropdown-wrapper");
 
-        let downloadBtn: Element = document.createElement("button");
-        downloadBtn.classList.add("btn", "btn-dark", "btn-sm", "dropdown-toggle");
-        downloadBtn.setAttribute("type", "button");
-        downloadBtn.setAttribute("data-bs-toggle", "dropdown");
-        downloadBtn.innerHTML = "DA25-Download";
+        let dropdownBtn: HTMLElement = document.createElement("button");
+        dropdownBtn.classList.add("dropdown-btn");
+        dropdownBtn.innerHTML = "DA25-Download";
+        dropdownWrapper.append(dropdownBtn);
 
-        dropdownWrapper.append(downloadBtn);
+        let itemListWrapper: HTMLElement = document.createElement("div");
+        itemListWrapper.style.display = "none";
+        dropdownWrapper.append(itemListWrapper);
 
-        let btnList: Element = document.createElement("ul");
-        btnList.classList.add("dropdown-menu", "btn-sm");
+        dropdownBtn.addEventListener("click", () => {
+            if (itemListWrapper.style.display === "none") {
+                itemListWrapper.style.display = "block";
+            } else {
+                if (itemListWrapper.style.display === "block" || itemListWrapper.style.display === "") {
+                    itemListWrapper.style.display = "none";
+                }
+            }
+        })
 
-        dropdownWrapper.append(btnList);
+        let itemList: HTMLElement = document.createElement("ul");
+        itemList.classList.add("dropdown-item-list");
+        itemListWrapper.append(itemList);
 
         console.log("Dropdown Element created");
         return dropdownWrapper;
     }
 
-    protected createDownloadEle(video: Video): Element {
-        let altAnchorEle: Element = document.createElement("a");
-        altAnchorEle.classList.add("dropdown-item", "btn-sm");
-        altAnchorEle.setAttribute("href", video.url);
-        altAnchorEle.innerHTML = video.text;
+    protected createDropdownItem(video: Video): HTMLElement {
+        let dropdownItemEle: HTMLElement = document.createElement("li");
+        dropdownItemEle.classList.add("dropdown-item");
+        dropdownItemEle.dataset["url"] = video.url;
+
+        dropdownItemEle.addEventListener("click", () => {
+            window.open(dropdownItemEle.dataset["url"], "_blank")?.focus();
+        })
+
+        let videoTextEle: HTMLElement = document.createElement("button");
+        videoTextEle.innerHTML = video.text;
+        dropdownItemEle.append(videoTextEle)
 
         console.log(`Download Element for: ${video.text} created with url:`, video.url);
-        return altAnchorEle;
+        return dropdownItemEle;
     }
 
-    protected appendDownloadToDropdown(dropdownEle: Element, downloadEle: Element): void {
-        let downloadBtnList: Element | null = dropdownEle.querySelector("ul.dropdown-menu");
-        let listItem: Element = document.createElement("li");
-        listItem.append(downloadEle);
-        if (downloadBtnList) {
-            downloadBtnList.append(listItem);
+    protected appendItemToDropdown(dropdownEle: HTMLElement, dropdownItem: HTMLElement): void {
+        let dropdownItemListEle: HTMLElement | null = dropdownEle.querySelector("ul.dropdown-item-list");
+        if (dropdownItemListEle) {
+            dropdownItemListEle.append(dropdownItem);
             console.log("Download Element appended to Dropdown");
         }
     }
 
-    protected attachDropdownToPage(dropdownEle: Element): void {
+    protected attachDropdownToPage(dropdownEle: HTMLElement): void {
         console.log("Attaching Dropdown Element to Page.")
-        let listEle: Element = document.createElement("li");
+        let listEle: HTMLElement = document.createElement("li");
         listEle.append(dropdownEle);
 
-        let menuList: Element | null = document.querySelector(".tabs-menu > ul");
+        let menuList: HTMLElement | null = document.querySelector(".tabs-menu > ul");
 
         if (!!menuList) {
             menuList.prepend(listEle);
@@ -72,23 +87,6 @@ export class UI {
             console.log(dropdownEle);
             // alert("window.flashvars present but '.tabs-menu > ul' is not");
         }
-    }
-
-    public addBootstrapToPage(): void {
-        let head: Element = document.getElementsByTagName("head")[0];
-        let bootstrapCssScriptEle: Element = document.createElement("link");
-        bootstrapCssScriptEle.setAttribute("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css");
-        bootstrapCssScriptEle.setAttribute("rel", "stylesheet");
-        bootstrapCssScriptEle.setAttribute("integrity", "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC");
-        bootstrapCssScriptEle.setAttribute("crossorigin", "anonymous");
-        head.insertAdjacentElement("beforeend", bootstrapCssScriptEle);
-
-        let body = document.getElementsByTagName("body")[0];
-        let bootstrapJsScriptEle = document.createElement("script");
-        bootstrapJsScriptEle.setAttribute("src", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js");
-        bootstrapJsScriptEle.setAttribute("integrity", "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM");
-        bootstrapJsScriptEle.setAttribute("crossorigin", "anonymous");
-        body.insertAdjacentElement("beforeend", bootstrapJsScriptEle);
     }
 
     public attachToPage(): void {
